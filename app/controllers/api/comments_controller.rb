a33a17a3afdc148223fb5aa683b1b10ac3a350f3
Josué Benavides <jdbs9514@gmail.com>
2023-02-02 19:48:39 -0500

    fix some controllers

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,5 +1,5 @@
 class Api::CommentsController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
   respond_to :json
 
